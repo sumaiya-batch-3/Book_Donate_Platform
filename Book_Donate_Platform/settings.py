0087df575067f71c159ba9cd17c7adb86f5cce40
Sumaiya Sharmin <ssumaiya@live.com>
2024-05-22 19:01:33 -0700

    database added

--- Book_Donate_Platform/settings.py
+++ Book_Donate_Platform/settings.py
@@ -31,7 +31,9 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
+CSRF_TRUSTED_ORIGINS = ['https://book-donate-platform-8re4.onrender.com','https://*.127.0.0.1']
+
 
 
 # Application definition
