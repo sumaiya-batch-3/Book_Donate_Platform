bde7985100b546c1fe63e195d950c9bda3a9c114
Sumaiya Sharmin <ssumaiya@live.com>
2024-05-22 22:47:15 -0700

    data change

--- Book_Donate_Platform/settings.py
+++ Book_Donate_Platform/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = env("SECRET_KEY")
 DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
-CSRF_TRUSTED_ORIGINS = ['https://book-donate-platform-8re4.onrender.com','https://*.127.0.0.1']
+CSRF_TRUSTED_ORIGINS = ['https://book-donate.onrender.com','https://*.127.0.0.1']
 
 
 
